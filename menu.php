f79cf095c7168c5b0a6d266cff6be33b82b264e8
TheLupert <gabriel_zero@live.com>
2020-09-24 16:41:24 -0300

    Arquivo menu.

---
+++ menu.php
@@ -0,0 +1,9 @@
+<header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Login</a></li>
+                <li><a href="exe1.php">Página de Produto</a></li>
+                <li><a href="exe2.php">Página de Categoria do Produto</a></li>
+            </ul>
+        </nav>
+</header>
